34b64e90b26bf3cdf9d02d6080d330794e1a729c
v-turin <vladimir.turin@opencascade.com>
2018-11-19 15:31:31 +0300

    Linux build

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# odm-feedstock
+About odm
+=========
+
+Home: https://www.opendronemap.org
+
+Package license: GPL 3.0
+
+Summary: A command line toolkit to generate maps, point clouds, 3D models and DEMs from drone, balloon or kite images.
---
+++ recipe/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ln -s ${PREFIX} SuperBuild/install
+
+mkdir build
+cd build
+
+export LDFLAGS="$LDFLAG -fuse-ld=gold -Wl,--no-as-needed"
+
+cmake -G "Ninja" \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX                  \
+      -DCMAKE_PREFIX_PATH=$PREFIX                     \
+      -DCMAKE_BUILD_TYPE:STRING=Release               \
+      -DODM_BUILD_SLAM:BOOL=ON                        \
+      -DBLA_VENDOR=Intel10_64lp                       \
+      -DCMAKE_LIBRARY_ARCHITECTURE=x86_64-linux-gnu   \
+      -DOpenGL_GL_PREFERENCE=GLVND                    \
+      ..
+
+ninja
+
+# Install
+cd ..
+
+cp build/bin/* ${PREFIX}/bin
+
+odm_dir=$SP_DIR/odm
+
+mkdir -p $odm_dir
+cp run.py $odm_dir
+cp settings.yaml $odm_dir
+cp VERSION $odm_dir
+
+cp -R scripts $odm_dir
+cp -R opendm $odm_dir
+
+slam_dir=modules/odm_slam/src
+mkdir -p $odm_dir/$slam_dir
+cp $slam_dir/*.py $odm_dir/$slam_dir
